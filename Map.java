cf110448e9ce3b3ec169745097cd639859856c8c
hugo lencastre <hugo.emanuel.lencastre@gmail.com>
2018-05-11 11:13:52 +0100

    Added base

---
+++ Map.java
@@ -0,0 +1,15 @@
+package aasma;
+
+import java.util.Vector;
+
+public class Map {
+
+    Vector<Vector<Object>> matrix = new Vector<>();
+    public Map(){
+
+    }
+
+    void update() {
+
+    }
+}
---
+++ Wall.java
@@ -0,0 +1,4 @@
+package aasma;
+
+public class Wall {
+}
